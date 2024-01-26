8240f9d9f552c2d16958e5b0c781e3dc6118a304
ZUHOWKS <jojo.vilard38@gmail.com>
2024-01-26 11:43:25 +0100

    Add game manager class to lead main action for the app game

---
+++ src/main/java/fr/zuhowks/game2048/game/GameManager.java
@@ -0,0 +1,34 @@
+package fr.zuhowks.game2048.game;
+
+import fr.zuhowks.game2048.game.stats.GameStatus;
+
+public class GameManager {
+
+    private GameStatus isInGame;
+    private Game2048 game2048;
+    private int score;
+
+    public GameManager() {
+        this.isInGame = GameStatus.NOT_IN_GAME;
+        this.game2048 = new Game2048();
+        this.score = 0;
+    }
+
+    public GameManager(Game2048 game2048) {
+        this.isInGame = GameStatus.NOT_IN_GAME;
+        this.game2048 = game2048;
+    }
+
+    public GameStatus getIsInGame() {
+        return isInGame;
+    }
+
+    public void startParty() {
+        //TODO: Setup party
+        this.isInGame = GameStatus.IN_GAME;
+    }
+
+    public void resetScore() {
+        this.score = 0;
+    }
+}
