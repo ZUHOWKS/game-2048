f4d22ad35840dd17f81802922e49bbb180cb65ad
ZUHOWKS <jojo.vilard38@gmail.com>
2024-02-09 09:34:35 +0100

    Extract method for box colors & pickup colors

--- src/main/java/fr/zuhowks/game2048/canvas/Canvas2048.java
+++ src/main/java/fr/zuhowks/game2048/canvas/Canvas2048.java
@@ -8,12 +8,14 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.RoundRectangle2D;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JPanel;
 
-public class Canvas2048 extends JPanel{
+public class Canvas2048 extends JPanel implements PropertyChangeListener {
 	
     //private final Jeu2048 jeu;
     private final int boxTaille; //reference Ã  la dicipline
@@ -23,25 +25,32 @@ public class Canvas2048 extends JPanel{
     Map<Integer, Color> colorBoxes = new HashMap<>();
     
     public Canvas2048(GameManager game) {
+
+
     	this.boxTaille =400;
     	this.game = game;
     	this.xDepart = 0;
     	this.yDepart = 0;
-    	this.colorBoxes.put(0, Color.LIGHT_GRAY);
-    	this.colorBoxes.put(2, new Color(237, 227, 217));
-    	this.colorBoxes.put(4, new Color(234, 223, 200));
-    	this.colorBoxes.put(8, new Color(240, 177, 123));
-    	this.colorBoxes.put(16, new Color(242, 148, 102));
-    	this.colorBoxes.put(32, new Color(243, 123, 97));
-    	this.colorBoxes.put(64, new Color(243, 93, 60));
-    	this.colorBoxes.put(128, new Color(235, 205, 113));
-    	this.colorBoxes.put(256, new Color(235, 203, 97));
-    	this.colorBoxes.put(512, new Color(234, 199, 80));
-    	this.colorBoxes.put(1024, new Color(237, 196, 63));
-    	this.colorBoxes.put(2048, new Color(237, 194, 46));
-    }
 
-@Override
+		setupBoxColors();
+	}
+
+	private void setupBoxColors() {
+		this.colorBoxes.put(0, new Color(203, 191, 178));
+		this.colorBoxes.put(2, new Color(237, 227, 217));
+		this.colorBoxes.put(4, new Color(234, 223, 200));
+		this.colorBoxes.put(8, new Color(240, 177, 123));
+		this.colorBoxes.put(16, new Color(242, 148, 102));
+		this.colorBoxes.put(32, new Color(243, 123, 97));
+		this.colorBoxes.put(64, new Color(243, 93, 60));
+		this.colorBoxes.put(128, new Color(235, 205, 113));
+		this.colorBoxes.put(256, new Color(235, 203, 97));
+		this.colorBoxes.put(512, new Color(234, 199, 80));
+		this.colorBoxes.put(1024, new Color(237, 196, 63));
+		this.colorBoxes.put(2048, new Color(237, 194, 46));
+	}
+
+	@Override
 protected void paintComponent(Graphics g) {
 	super.paintComponent(g);
 	Graphics2D g2D = (Graphics2D) g;
@@ -51,9 +60,19 @@ protected void paintComponent(Graphics g) {
 public RoundRectangle2D createRoundedRect(int x, int y, double w, double h, Graphics g2D) {
     return new RoundRectangle2D.Double(x, y, w, h, 15, 15);
 }
-public void Boxes(int x, int y, Graphics2D g2D, int nb) {
+
+
+	/**
+	 * Generate boxes
+	 *
+	 * @param x | position x
+	 * @param y | position y
+	 * @param g2D | Graphics2D to generate rounded box
+	 * @param nb | number which is contained by the box.
+	 */
+public void generateBox(int x, int y, Graphics2D g2D, int nb) {
 	
-	double squareSize = 9*boxTaille/40;
+	double squareSize = (double) (9 * boxTaille) /40;
 	String numberAff = ""+nb;
 	float sizeFont = (float) (squareSize/60);
 	
@@ -86,7 +105,7 @@ public void grille(int x, int y, Graphics2D g2D) {
 
 	for(int i = 0; i < 4; i++) {
 		for(int j = 0; j < 4; j++) {
-			Boxes(x, y, g2D, grilleClient[i][j]);
+			generateBox(x, y, g2D, grilleClient[i][j]);
 			x += jumpBoxes;	
 		}
 		x = xDepart + spaceLines;
@@ -97,7 +116,7 @@ public void grille(int x, int y, Graphics2D g2D) {
 }
 public void score(int x, int y, int nbScore, Float sizeFont, Graphics2D g2D) {
 	String scoreAffMax = "Score: 99999";
-	g2D.setColor(Color.BLACK);
+	g2D.setColor(new Color(186, 172, 159));
 	g2D.setFont(g2D.getFont().deriveFont(g2D.getFont().getSize() * sizeFont));
 	FontMetrics metrics = g2D.getFontMetrics();
 	g2D.setStroke(new BasicStroke(sizeFont));
@@ -111,4 +130,9 @@ public void score(int x, int y, int nbScore, Float sizeFont, Graphics2D g2D) {
 	g2D.setFont(g2D.getFont().deriveFont(g2D.getFont().getSize() / sizeFont));
 	
 }
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		this.repaint();
+	}
 }
