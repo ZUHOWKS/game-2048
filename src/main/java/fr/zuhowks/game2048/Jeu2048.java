ec909260b13ef6d32e140d0f2a0ba6ba250baa2b
ZUHOWKS <jojo.vilard38@gmail.com>
2024-02-09 09:19:35 +0100

    Rename class

--- src/main/java/fr/zuhowks/game2048/Jeu2048.java
+++
@@ -1,5 +0,0 @@
-package fr.zuhowks.game2048;
-
-public class Jeu2048 {
-
-}
--- src/main/java/fr/zuhowks/game2048/windows/MainWindow.java
+++ src/main/java/fr/zuhowks/game2048/windows/MainWindow.java
@@ -1,6 +1,21 @@
 package fr.zuhowks.game2048.windows;
 
+import fr.zuhowks.game2048.canvas.Canvas2048;
+import fr.zuhowks.game2048.game.GameManager;
+
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
+    private GameManager game;
+    public static final int HEIGHT = 800;
+    public static final int WIDTH = 800;
+    Canvas2048 canvas;
+    public MainWindow() {
+        this.game = new GameManager();
+        this.canvas = new Canvas2048(this.game);
+
+        this.setSize(WIDTH,HEIGHT);
+        this.add(canvas);
+
+    }
 }
