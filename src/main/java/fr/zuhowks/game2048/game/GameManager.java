97a0fb17e31fc8d59d33f1df2b883579edf50c87
ZUHOWKS <jojo.vilard38@gmail.com>
2024-02-09 09:56:53 +0100

    Add property change support for game status

--- src/main/java/fr/zuhowks/game2048/game/GameManager.java
+++ src/main/java/fr/zuhowks/game2048/game/GameManager.java
@@ -4,16 +4,22 @@ import fr.zuhowks.game2048.game.client.ClientGame;
 import fr.zuhowks.game2048.game.server.ServerGame;
 import fr.zuhowks.game2048.game.server.stats.GameStatus;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class GameManager {
 
     private GameStatus gameStatus;
     private ServerGame serverGame;
     private ClientGame clientGame;
 
+    private PropertyChangeSupport pcs;
+
     public GameManager() {
         this.gameStatus = GameStatus.NOT_IN_GAME;
         this.serverGame = new ServerGame();
         this.clientGame = new ClientGame();
+        this.pcs = new PropertyChangeSupport(this);
     }
 
     public GameManager(ServerGame serverGame) {
@@ -45,10 +51,12 @@ public class GameManager {
                         this.serverGame.moveLeft();
                     }
                 }
-
+                updateGameStatus();
                 this.clientGame.update(this.serverGame.getGridCopy(), this.serverGame.getScore());
             }
         }
+
+
 
         //TODO: Action possible in the another game status.
 
@@ -59,7 +67,9 @@ public class GameManager {
         this.serverGame.resetScore();
         this.serverGame.generateRandomBox();
 
+        GameStatus oldStatus = this.gameStatus;
         this.gameStatus = GameStatus.IN_GAME;
+        pcs.firePropertyChange("gameStatus", oldStatus, this.gameStatus);
     }
 
     public boolean nextPlay() {
@@ -78,11 +88,32 @@ public class GameManager {
         return false;
     }
 
+    public void updateGameStatus() {
+        if (this.serverGame.isGridFinished()) {
+            this.gameStatus = GameStatus.WIN;
+            pcs.firePropertyChange("gameStatus", GameStatus.IN_GAME, this.gameStatus);
+        } else if (!nextPlay()) {
+            this.gameStatus = GameStatus.LOOSE;
+            pcs.firePropertyChange("gameStatus", GameStatus.IN_GAME, this.gameStatus);
+        }
+    }
+
     public void leaveParty() {
+        GameStatus oldStatus = this.gameStatus;
         this.gameStatus = GameStatus.NOT_IN_GAME;
+        pcs.firePropertyChange("gameStatus", oldStatus, this.gameStatus);
     }
 
     public ClientGame getClientGame() {
         return clientGame;
     }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.pcs.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        this.pcs.removePropertyChangeListener(listener);
+    }
+
 }
