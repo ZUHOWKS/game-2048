998ced8d1cf5f8a2181e5da916bd408de16243de
ZUHOWKS <jojo.vilard38@gmail.com>
2024-02-09 10:01:21 +0100

    Add keyboard listener to perform action with key

---
+++ src/main/java/fr/zuhowks/game2048/listeners/KeyBoardCommandListener.java
@@ -0,0 +1,44 @@
+package fr.zuhowks.game2048.listeners;
+
+import fr.zuhowks.game2048.game.GameManager;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoardCommandListener implements KeyListener {
+
+    GameManager game;
+
+    public KeyBoardCommandListener(GameManager game) {
+        this.game = game;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        switch( keyCode ) {
+            case KeyEvent.VK_UP:
+                game.performAction("moveUp");
+                break;
+            case KeyEvent.VK_DOWN:
+                game.performAction("moveDown");
+                break;
+            case KeyEvent.VK_LEFT:
+                game.performAction("moveLeft");
+                break;
+            case KeyEvent.VK_RIGHT :
+                game.performAction("moveRight");
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
